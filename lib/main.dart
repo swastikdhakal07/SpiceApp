d4ae8a0a614aa212542cdb7a918f522426945f83
unknown <dhakalswastik11@gmail.com>
2026-09-17 11:28:35 +0200

    small improvement in product card UI

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Test data
     final products = [
-      'Turmeric Powder',
-      'Cumin Powder',
-      'Coriander Powder',
-      'Chili Powder',
+      {'name': 'Turmeric Powder', 'weight': '500 g', 'price': 3.99},
+      {'name': 'Cumin Powder', 'weight': '250 g', 'price': 3.99},
+      {'name': 'Coriander Powder', 'weight': '250 g', 'price': 3.99},
+      {'name': 'Chili Powder', 'weight': '250 g', 'price': 3.99},
     ];
 
     return Scaffold(
@@ -38,7 +39,25 @@ class HomeScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, index) {
-          return ListTile(title: Text(products[index]));
+          return Card(
+            //creats a card with a margin of 8 pixels on all sides for demo
+            margin: const EdgeInsets.all(8.0),
+            child: ListTile(
+              //placeholder Icon as product image
+              leading: const Icon(Icons.shopping_bag),
+              //Name of the product in bold
+              title: Text(
+                products[index]['name'].toString(),
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(products[index]['weight'].toString()),
+              trailing: Text(
+                '€${products[index]['price']}',
+                //Price of the product in bold
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          );
         },
       ),
     );
